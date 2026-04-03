b1e73d16dfe9927419a162222c80bfb6e8d1ee31
rebemybk971 <mulciba.rbk@gmail.com>
2026-04-03 17:15:46 -0400

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import VueJourJPage from './pages/VueJourJPage'
 import HistoriquePage from './pages/HistoriquePage'
 import MembresPage from './pages/MembresPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
+import InscriptionPage from './pages/InscriptionPage'
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useAuth()
@@ -22,22 +23,27 @@ function ProtectedRoute({ children }) {
 export default function App() {
   const { user, loading } = useAuth()
   if (loading) return <div className="loading">Chargement…</div>
-  if (!user) return <LoginPage />
+
   return (
     <Routes>
       <Route path="/reset-password" element={<ResetPasswordPage />} />
-      <Route element={<Layout />}>
-        <Route index element={<Navigate to="/repertoire" replace />} />
-        <Route path="/repertoire" element={<RepertoirePage />} />
-        <Route path="/repertoire/ajouter" element={<AddSongPage />} />
-        <Route path="/repertoire/:id" element={<SongDetailPage />} />
-        <Route path="/evenements" element={<EvenementsPage />} />
-        <Route path="/evenements/:id" element={<EvenementDetailPage />} />
-        <Route path="/evenements/:id/jour-j" element={<VueJourJPage />} />
-        <Route path="/historique" element={<HistoriquePage />} />
-        <Route path="/membres" element={<MembresPage />} />
-        <Route path="*" element={<Navigate to="/repertoire" replace />} />
-      </Route>
+      <Route path="/inscription" element={<InscriptionPage />} />
+      {!user ? (
+        <Route path="*" element={<LoginPage />} />
+      ) : (
+        <Route element={<Layout />}>
+          <Route index element={<Navigate to="/repertoire" replace />} />
+          <Route path="/repertoire" element={<RepertoirePage />} />
+          <Route path="/repertoire/ajouter" element={<AddSongPage />} />
+          <Route path="/repertoire/:id" element={<SongDetailPage />} />
+          <Route path="/evenements" element={<EvenementsPage />} />
+          <Route path="/evenements/:id" element={<EvenementDetailPage />} />
+          <Route path="/evenements/:id/jour-j" element={<VueJourJPage />} />
+          <Route path="/historique" element={<HistoriquePage />} />
+          <Route path="/membres" element={<MembresPage />} />
+          <Route path="*" element={<Navigate to="/repertoire" replace />} />
+        </Route>
+      )}
     </Routes>
   )
 }
