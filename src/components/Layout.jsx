9838af95fd2c8bb50aa2e45284e4dca1628384ed
rebemybk971 <mulciba.rbk@gmail.com>
2026-04-04 10:56:17 -0400

    Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -39,7 +39,7 @@ export default function Layout() {
   const isJourJ = location.pathname.includes('/jour-j')
   async function handleSignOut() {
     await signOut()
-    window.location.href = '/'
+    window.location.reload()
   }
   return (
     <>
